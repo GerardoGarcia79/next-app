0a647d16c63b0a1cd979bf787781d6725645e98b
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-11-29 09:51:14 -0600

    Dynamic routes

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,15 @@
+import Photos from "./photos/page";
+
+interface Props {
+  params: { id: number };
+}
+
+const UserDetailPage = ({ params: { id } }: Props) => {
+  return (
+    <>
+      <div>UserDetailPage {id}</div>
+    </>
+  );
+};
+
+export default UserDetailPage;
