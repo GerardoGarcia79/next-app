776241a6981068c3d0ad9e6c2ad07804b5d84abe
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-11-29 09:02:00 -0600

    Routing overview

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,36 +1,10 @@
-import React from "react";
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import UserTable from "./UserTable";
 
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 10 },
-  });
-  const users: User[] = await res.json();
-
   return (
     <>
       <h1>Users</h1>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((u) => (
-            <tr key={u.id}>
-              <td>{u.name}</td>
-              <td>{u.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   );
 };
