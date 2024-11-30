6faa21462a814fdaf248ef9fb24ff5466d3a65a0
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-11-30 08:41:36 -0600

    Creating an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,10 @@ export function GET(request: NextRequest) {
     { id: 2, name: "Gerard" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json(); //Read body of the response
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 404 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
