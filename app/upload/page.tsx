593ea4d8a34054771add10c06394aea7af40a62f
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-12-06 13:44:20 -0600

    Showing uploaded images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,17 +1,32 @@
 "use client";
-
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget uploadPreset="gdbybt2f">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt="An image" />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="gdbybt2f"
+        onSuccess={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
