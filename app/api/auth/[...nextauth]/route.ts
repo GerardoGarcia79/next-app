661787a29bf9c47bfcdafb165fcfd35f6cdb44e7
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-12-07 00:13:59 -0600

    Accessing session on the server

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,15 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-});
+};
 
-// Export function with 2 different names, this means that
-// any GET or POST action, will be handled inside this handler function
+const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
