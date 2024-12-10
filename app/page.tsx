5bb6b0846e6b9b4dd73cd51ce3b2b4505b9156bd
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-12-10 11:10:38 -0600

    Lazy loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,21 @@
-import { Metadata } from "next";
-import Image from "next/image";
+"use client";
 
-export default async function Home() {
+export default function Home() {
   return (
     <main className="relative h-screen">
-      {/* <Image
-        src="https://bit.ly/react-cover"
-        alt="Camera"
-        fill
-        className="object-cover"
-        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-        quality={100}
-        priority
-      /> */}
       <h1>Hello World</h1>
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+          const sorted = _.orderBy(users, ["name"]);
+
+          console.log(sorted);
+        }}
+      >
+        Show
+      </button>
     </main>
   );
-}
-
-export async function generateMetadata(): Promise<Metadata> {
-  const product = await fetch("");
-
-  return {
-    title: "",
-    description: "",
-  };
 }
